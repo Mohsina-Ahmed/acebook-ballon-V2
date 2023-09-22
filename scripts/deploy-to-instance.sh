6505d0eb75eb08f7c91c766ee1ebc9fdfb5effc2
Mohsina-Ahmed <mohsinaahmedchowdhury@gmail.com>
2023-09-22 16:36:47 +0100

    Adding deploy on yml file

--- scripts/deploy-to-instance.sh
+++ scripts/deploy-to-instance.sh
@@ -9,5 +9,5 @@ chmod 700 ~/.ssh
 # The instance ip address is 52.12.34.133
 # we should probably avoid hardcoding this, in case
 # the IP changes
-rsync -av -e "ssh -o StrictHostKeyChecking=no" ./* ec2-user@52.12.34.133:/var/acebook/
-ssh -o StrictHostKeyChecking=no ec2-user@52.12.34.133 "sudo systemctl restart acebook"
+rsync -av -e "ssh -o StrictHostKeyChecking=no" ./* ec2-user@$BALLOON_INSTANCE_IP_1:/var/acebook/
+ssh -o StrictHostKeyChecking=no ec2-user@$BALLOON_INSTANCE_IP_1 "sudo systemctl restart acebook"
